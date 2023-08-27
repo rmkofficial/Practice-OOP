fb446b2e656ab4b4e1eb077dce4504a9c958dc3a
kmert0@outlook.com <kmert0@outlook.com>
2023-08-27 17:02:47 +0300

    Switch Project Logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,18 +1,42 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
+  connectMoreInfoButton() {}
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+  }
+}
 
 class ProjectList {
+  projects = [];
+
   constructor(type) {
-    const prjItems = document.querySelectorAll('#${type}-projects li');
+    const prjItems = document.querySelectorAll("#${type}-projects li");
     console.log(prjItems);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+  addProject() {}
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    this.addProject();
+    this.projects = this.projects.filter((p) => p.id !== projectId);
   }
 }
 
 class App {
   static init() {
-    const activeProjectsList = new ProjectList('active');
-    const finishedProjectsList = new ProjectList('finished');
+    const activeProjectsList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
   }
 }
 
